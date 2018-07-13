6fb4573776c02e7b657251255c6d37bda6165a09
Robin Noyel <greenplop@gmail.com>
2018-07-14 00:41:11 +0200

    feat(app.comp) create test request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Request } from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.test();
+  }
+
+  public test() {
+    let request: Request = {
+      body: null,
+      headers: [{
+        key: 'key',
+        value: 'value',
+      }],
+      method: 'GET',
+      response: {
+        text: '{"status":200,"description":"ok"}',
+        status: 200,
+      },
+      url: 'https://api.travian.engin9tools.com/api/global/servers',
+    }
+    //send request to service
+  }
 }
