7047ee0b1dea1131be1ab6381e6438ad6f3609c1
Robin Noyel <greenplop@gmail.com>
2018-07-14 13:05:07 +0200

    create tester component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RequestService } from './request.service';
+import { TesterComponent } from './tester/tester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesterComponent
   ],
   imports: [
     BrowserModule,
