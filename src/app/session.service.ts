4158bdac73398eb69869892b3d1a2264b1988958
Robin Noyel <greenplop@gmail.com>
2018-07-17 19:04:59 +0200

    save tests

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -10,15 +10,21 @@ import { isArray } from 'util';
 export class SessionService {
 
   public tests$ = new BehaviorSubject<HttpTest[]>([]);
+  private data: Data = {
+    version: 1,
+    tests: [],
+  }
 
   constructor() {
     console.log('constructor');
     let data = Storage.get<Data>(Storage.KEY_TESTS, null);
     if (data) {
-      if (data.tests) {
-        if (isArray(typeof data.tests)) {
-          // TODO typecheck httpTests
-          this.tests$.next(data.tests);
+      if (data.version == 1) {
+        if (data.tests) {
+          if (isArray(data.tests)) {
+            // TODO typecheck httpTests
+            this.tests$.next(data.tests);
+          }
         }
       }
     }
@@ -28,5 +34,11 @@ export class SessionService {
     let tests = this.tests$.value;
     tests.push(test);
     this.tests$.next(tests);
+  }
+
+  public saveTests(tests: HttpTest[]) {
+    this.data.tests = tests;
+    this.tests$.next(this.data.tests);
+    Storage.set(Storage.KEY_TESTS, this.data);
   }
 }
