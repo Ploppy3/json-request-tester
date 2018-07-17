602f7cd28b692062ddb15dd75fb70b01be2aa3fd
Robin Noyel <greenplop@gmail.com>
2018-07-17 17:15:14 +0200

    feat(test-generator.comp) implement reactive form

--- src/app/test-generator/test-generator.component.ts
+++ src/app/test-generator/test-generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-test-generator',
@@ -9,17 +10,29 @@ export class TestGeneratorComponent implements OnInit {
 
   public formVisible = false;
   //public testObj = {};
-  //*
+
   public testObj = {
     "status": 200,
     "description": "%any_string%",
     "data": "%any_array%"
   }
-  //*/
+  public formTest = new FormGroup({
+    url: new FormControl('https://api.travian.engin9tools.com/api/global/servers'),
+    method: new FormControl('GET'),
+  })
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+class Model {
+  url: string;
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  body: any;
+  expectedResponse: {
+    body: {};
+    status: number
+  };
 }
