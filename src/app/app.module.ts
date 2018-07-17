6ffa0ae46e927976d19f82089b316f7b8de97aa7
Robin Noyel <greenplop@gmail.com>
2018-07-17 17:13:26 +0200

    rename tester.comp => home.comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TestService } from './test.service';
 import { RequestCardComponent } from './request-card/request-card.component';
-import { TesterComponent } from './tester/tester.component';
+import { HomeComponent } from './home/home.component';
 import { ToJson } from './tojson.pipe';
 import { TestGeneratorComponent } from './test-generator/test-generator.component';
 import { JsonEditorComponent } from './test-generator/json-editor/json-editor.component';
@@ -19,7 +19,7 @@ import { SessionService } from './session.service';
   declarations: [
     AppComponent,
     RequestCardComponent,
-    TesterComponent,
+    HomeComponent,
     ToJson,
     TestGeneratorComponent,
     JsonEditorComponent,
@@ -31,6 +31,7 @@ import { SessionService } from './session.service';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [ TestService, SessionService ],
   bootstrap: [AppComponent]
