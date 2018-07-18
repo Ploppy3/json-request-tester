ae02b74c891cd0fae85b49dfdecccb80e6f6ef83
Robin Noyel <greenplop@gmail.com>
2018-07-18 15:20:16 +0200

    feat(animations) add collapse animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -11,3 +11,16 @@ export const fadeInOut = trigger('fadeInOut', [
     animate('500ms ease-out', style({ opacity: 0, })),
   ]),
 ]);
+
+export const collapse = trigger('collapse', [
+  transition(':enter', [
+    style({ height: 0, paddingTop: 0, paddingBottom: 0, marginTop: 0, marginBottom: 0, opacity: 0 }),
+    animate('.25s ease-in', style({ height: '*', paddingTop: '*', paddingBottom: '*', marginTop: '*', marginBottom: '*' })),
+    animate('.25s ease-out', style({ opacity: 1 })),
+  ]),
+  transition(':leave', [
+    animate('.25s ease-in', style({ opacity: 0 })),
+    animate('.25s ease-out', style({ height: 0, paddingTop: 0, paddingBottom: 0, marginTop: 0, marginBottom: 0 })),
+  ]),
+  transition('collapsed <=> expanded', animate('500ms ease-in-out')),
+]);
