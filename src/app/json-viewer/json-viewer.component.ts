87bb0531dd94ad11187380029c69cd294e957590
Robin Noyel <greenplop@gmail.com>
2018-07-18 12:47:04 +0200

    fix(test-generator) prevent corruption by emitting copy of object

--- src/app/json-viewer/json-viewer.component.ts
+++ src/app/json-viewer/json-viewer.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
-import { JsonEditorComponent } from './json-editor/json-editor.component';
 
 @Component({
   selector: 'app-json-viewer',
@@ -9,7 +8,7 @@ import { JsonEditorComponent } from './json-editor/json-editor.component';
 export class JsonViewerComponent implements OnInit, OnChanges {
 
   @Input() obj;
-  @Output() change = new EventEmitter<void>();
+  @Output() objChange = new EventEmitter<{}>();
   public copyOfObj;
 
   constructor() { }
@@ -26,7 +25,7 @@ export class JsonViewerComponent implements OnInit, OnChanges {
 
   public saveChanges() {
     this.obj = JSON.parse(JSON.stringify(this.copyOfObj));
-    this.change.emit();
+    this.objChange.emit(JSON.parse(JSON.stringify(this.obj)));
   }
 
   public discardChanges() {
